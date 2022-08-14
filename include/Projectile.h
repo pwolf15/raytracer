367fc97238e73196a87c467af021139b8902db73
pwolf15 <patrick.wolf@live.com>
2022-08-13 17:01:28 -0700

    Added 'Projectile'

---
+++ include/Projectile.h
@@ -0,0 +1,16 @@
+#ifndef PROJECTILE_H
+#define PROJECTILE_H
+
+#include "Point.h"
+#include "Vector.h"
+
+class Projectile 
+{
+public:
+  Point position;
+  Vector velocity;
+
+private:
+};
+
+#endif // PROJECTILE_H
